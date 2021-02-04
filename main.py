3a5a51cb557c22e0580506487c8244108c3f9487
KatyMartin23 <832006@mystma.org>
2021-02-04 18:53:15 +0000

    letter pos 1

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#WRITE YOUR CODE IN THIS FILE
+def letterPos(strWord, strLetter):
+    strTest = ""
+    intreturn = 0
+    for i in range (0, len(strWord)):
+        strTest = strWord[i]
+        if strTest == strLetter:
+            return i + 1
+    return 0
+
+    print("katy was here")
+
+    #print(letterPos("katy", "t"))
